a749eefe9a303dd5d59b18ec4cf7d096403fc0f1
oat431 <oat431@gmail.com>
2023-10-16 22:06:05 +0700

    🔧 update: register eureka

--- src/main/java/panomete/playground/demogql/DemogqlApplication.java
+++ src/main/java/panomete/playground/demogql/DemogqlApplication.java
@@ -2,8 +2,10 @@ package panomete.playground.demogql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemogqlApplication {
 
     public static void main(String[] args) {
